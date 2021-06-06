07f560c5a1cd85fb15bc9a28a4310c71f43e47e6
Andrey <dydyshko1999@gmail.com>
2021-06-06 15:38:26 +0300

    Implement admin product list, create and update product functionality

--- backend/shop/urls/product_urls.py
+++ backend/shop/urls/product_urls.py
@@ -5,5 +5,10 @@ from shop.views import product_views as views
 
 urlpatterns = [
     path('', views.get_products, name='products'),
+    path('create/', views.create_product, name='product-create'),
+    path('upload/', views.upload_image, name='image-upload'),
+
     path('<str:pk>/', views.get_product, name='product'),
+    path('delete/<str:pk>/', views.delete_product, name='product-delete'),
+    path('update/<str:pk>/', views.update_product, name='product-update'),
 ]
--- backend/shop/views/product_views.py
+++ backend/shop/views/product_views.py
@@ -19,3 +19,61 @@ def get_product(request, pk):
     product = Product.objects.get(_id=pk)
     serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def delete_product(request, pk):
+    product = Product.objects.get(_id=pk)
+    product.delete()
+    return Response('Product was deleted')
+
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def create_product(request):
+    user = request.user
+    product = Product.objects.create(
+        user=user,
+        name='Sample Name',
+        price=0,
+        brand='Sample Brand',
+        category='Sample Category',
+        countInStock=0,
+        description=''
+    )
+
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def update_product(request, pk):
+    data = request.data
+    product = Product.objects.get(_id=pk)
+
+    product.name = data['name']
+    product.price = data['price']
+    product.category = data['category']
+    product.brand = data['brand']
+    product.countInStock = data['countInStock']
+    product.description = data['description']
+
+    product.save()
+
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def upload_image(request):
+    data = request.data
+    
+    product_id = data['product_id']
+    product = Product.objects.get(_id=product_id)
+
+    product.image = request.FILES.get('image')
+    product.save()
+
+    return Response('Image was uploaded')
